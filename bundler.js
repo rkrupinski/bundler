b0de8f7c5b63b29467fdff360c5e91ca4c997eb1
Rafał Krupiński <krupinski.rafal@gmail.com>
2018-06-18 22:38:21 +0200

    should work now

---
+++ bundler.js
@@ -0,0 +1,36 @@
+const { readFileSync } = require('fs');
+const { dirname, join } = require('path');
+const { parse } = require('@babel/parser');
+const { transformFromAst } = require('@babel/core');
+const traverse = require('@babel/traverse').default;
+
+let ID = 0;
+
+function createAsset(filename) {
+  const content = readFileSync(filename, 'utf-8');
+
+  const ast = parse(content, { sourceType: 'module' });
+
+  const dependencies = [];
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    },
+  });
+
+  const id = ID++;
+
+  const { code } = transformFromAst(ast, null, {
+    presets: ['env'],
+  });
+
+  return {
+    id,
+    filename,
+    dependencies,
+    code,
+  };
+}
+
+createAsset('./example/entry.js');
